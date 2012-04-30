1b368c8930d67ff201a642a36851dcd316ba877e
Steven Mondelaers <steven.mondelaers@gmail.com>
2012-04-30 16:21:16 +0200

    New layout

--- index.php
+++ index.php
@@ -2,37 +2,30 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		
-		
-        <?php
-          include_once('includes/includeHead.php');
-        ?>
-		
+
+		<?php
+        include_once ('includes/includeHead.php');
+		?>
+
 		<title>Home | <?php echo siteName; ?></title>
-		
+
 	</head>
 	<body>
-	    <?php
-	       include_once('includes/includeHeader.php');
-	    ?>
-	    <div class="wrapper">
-	        <?php
-	           include_once('includes/includeNavigation.php');
-	        ?>
-<<<<<<< HEAD
-            <section class="wrapper">
-                
-=======
-            <section>
-                <?php
-                	echo "Hello world";
-                ?>
->>>>>>> 038ec898c32cc13b6313b5a0f0a166f0006c80ea
-            </section>
-            
-	    </div>
-	    <?php
-	       include_once('includes/includeFooter.php');
-	    ?>
+		<?php
+        include_once ('includes/includeHeader.php');
+		?>
+		<div class="wrapper">
+			<?php
+            include_once ('includes/includeNavigation.php');
+			?>
+
+			<section class="wrapper">
+
+			</section>
+
+		</div>
+		<?php
+        include_once ('includes/includeFooter.php');
+		?>
 	</body>
 </html>
