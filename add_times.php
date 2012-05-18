7c648ea2acbfce047dad296e0b79d4215814728d
stevenMondelaers <steven.mondelaers@gmail.com>
2012-05-19 01:17:01 +0300

    Update add_times.php

--- add_times.php
+++ add_times.php
@@ -1,5 +1,31 @@
 <?php
 session_start();
+
+if (!empty($_POST['competition'])) {
+    require_once 'classes/time.class.php';
+
+    try {
+        $time = new Time();
+
+        $time -> Competition = $_POST['competition'];
+        $time -> Distance = $_POST['distance'];
+		$time -> TimeFull = (($_POST['tijdHun']) + (100*$_POST['tijdSec']) + ((60*$_POST['tijdMin'])*100));
+		$time -> ReactionFull = (($_POST['reactionHun']) + (100*$_POST['reactionSec']));
+		$time -> Time50 = (($_POST['tijd50Hun']) + (100*$_POST['tijd50Sec']) + ((60*$_POST['tijd50Min'])*100));
+		$time -> Time100 = (($_POST['tijd100Hun']) + (100*$_POST['tijd100Sec']) + ((60*$_POST['tijd100Min'])*100));
+		$time -> Time200 = (($_POST['tijd200Hun']) + (100*$_POST['tijd200Sec']) + ((60*$_POST['tijd200Min'])*100));
+		$time -> Time400 = (($_POST['tijd400Hun']) + (100*$_POST['tijd400Sec']) + ((60*$_POST['tijd400Min'])*100));
+		$time -> Time800 = (($_POST['tijd800Hun']) + (100*$_POST['tijd800Sec']) + ((60*$_POST['tijd800Min'])*100));
+		$time -> Time1500 = (($_POST['tijd1500Hun']) + (100*$_POST['tijd1500Sec']) + ((60*$_POST['tijd1500Min'])*100));
+
+        $time -> timeReg();
+        $feedback = "Success";
+
+    } catch (exception $e) {
+        $feedback = $e->getMessage();
+    }
+}
+
 include_once ('classes/database.class.php');
 $db = new Database();
 
@@ -21,7 +47,7 @@ $wedstrijden = $db -> selectCompetitions();
 		<?php
         include_once ('includes/includeHead.php');
 		?>
-		<title>Register | <?php echo siteName; ?></title>
+		<title>Add Times | <?php echo siteName; ?></title>
 
 	</head>
 	<body>
@@ -33,16 +59,16 @@ $wedstrijden = $db -> selectCompetitions();
             include_once ('includes/includeNavigation.php');
 			?>
 			<section>
-				<form>
+				<form method="post" action="<?php echo 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']; ?>">
 					<div id="basicTime">
 						<fieldset>
 							<legend>Time registration</legend>
 							<ul>
 								<li>
 									<label for="wedstrijd">Competition:</label>
-									<select>
+									<select id="competition" name="competition">
 										<?php
-										while($teller < 5 && $wedstrijd = $wedstrijden -> fetch_array()){
+										while($teller < 10 && $wedstrijd = $wedstrijden -> fetch_array()){
 										?>
 										<option	value="<?php echo $wedstrijd['WedstrijdID']; ?>"
 										<?php
@@ -58,7 +84,7 @@ $wedstrijden = $db -> selectCompetitions();
 								</li>
 								<li>
 									<label for="afstand">Distance:</label>
-									<select>
+									<select id="distance" name="distance">
 										<?php
 										while ($afstand = $afstanden -> fetch_array()) {
 										?>
@@ -75,15 +101,15 @@ $wedstrijden = $db -> selectCompetitions();
 									</select>
 								</li>
 								<li>
-									<label for="tijd">Time:</label>
-									<input name="tijd" class="tijd" type="text" placeholder="mm"/>
-									<input name="tijd" class="tijd" type="text" placeholder="ss"/>
-									<input name="tijd" class="tijd" type="text" placeholder="ff"/>
+									<label>Time:</label>
+									<input name="tijdMin" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijdSec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijdHun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="reactie">Reaction Time:</label>
-									<input name="reactie" class="tijd" type="text" placeholder="ss"/>
-									<input name="reactie" class="tijd" type="text" placeholder="ff"/>
+									<label>Reaction Time:</label>
+									<input name="reactionSec" class="tijd" type="text" placeholder="ss"/>
+									<input name="reactionHun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 							</ul>
 						</fieldset>
@@ -93,40 +119,40 @@ $wedstrijden = $db -> selectCompetitions();
 							<legend>Split times</legend>
 							<ul>
 								<li>
-									<label for="m50">50 meter:</label>
-									<input name="m50" class="tijd" type="text" placeholder="mm"/>
-									<input name="m50" class="tijd" type="text" placeholder="ss"/>
-									<input name="m50" class="tijd" type="text" placeholder="ff"/>
+									<label>50 meter:</label>
+									<input name="tijd50Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd50Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd50Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="m100">100 meter:</label>
-									<input name="m100" class="tijd" type="text" placeholder="mm"/>
-									<input name="m100" class="tijd" type="text" placeholder="ss"/>
-									<input name="m100" class="tijd" type="text" placeholder="ff"/>
+									<label>100 meter:</label>
+									<input name="tijd100Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd100Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd100Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="m200">200 meter:</label>
-									<input name="m200" class="tijd" type="text" placeholder="mm"/>
-									<input name="m200" class="tijd" type="text" placeholder="ss"/>
-									<input name="m200" class="tijd" type="text" placeholder="ff"/>
+									<label>200 meter:</label>
+									<input name="tijd200Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd200Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd200Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="m400">400 meter:</label>
-									<input name="m400" class="tijd" type="text" placeholder="mm"/>
-									<input name="m400" class="tijd" type="text" placeholder="ss"/>
-									<input name="m400" class="tijd" type="text" placeholder="ff"/>
+									<label>400 meter:</label>
+									<input name="tijd400Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd400Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd400Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="m800">800 meter:</label>
-									<input name="m800" class="tijd" type="text" placeholder="mm"/>
-									<input name="m800" class="tijd" type="text" placeholder="ss"/>
-									<input name="m800" class="tijd" type="text" placeholder="ff"/>
+									<label>800 meter:</label>
+									<input name="tijd800Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd800Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd800Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
-									<label for="m1500">1500 meter:</label>
-									<input name="m1500" class="tijd" type="text" placeholder="mm"/>
-									<input name="m1500" class="tijd" type="text" placeholder="ss"/>
-									<input name="m1500" class="tijd" type="text" placeholder="ff"/>
+									<label>1500 meter:</label>
+									<input name="tijd1500Min" class="tijd" type="text" placeholder="mm"/>
+									<input name="tijd1500Sec" class="tijd" type="text" placeholder="ss"/>
+									<input name="tijd1500Hun" class="tijd" type="text" placeholder="ff"/>
 								</li>
 								<li>
 									<input type="submit" value="Submit" class="btn-time" />
